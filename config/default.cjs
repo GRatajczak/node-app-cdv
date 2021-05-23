8a5b166872e0589d76e58d54b8fcbc81463eb2ff
Grzegorz <g.ratajczak97@gmail.com>
2021-05-23 19:07:12 +0200

    added securyty and endpoints

--- config/default.cjs
+++ config/default.cjs
@@ -6,4 +6,8 @@ module.exports = {
 	is_test: false,
 	name: "restaurant-manager-js",
 	database: databaseConfig,
+	options: {
+		port: process.env.PORT || 3001,
+		public_routes_prefix: "/api/public",
+	},
 };
--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
+const fs = require('fs')
+const https = require('https')
+const util = require('util')
+const limit = require('express-rate-limit')
 const bodyParser = require('body-parser')
-const helmet = require('helmet');
+const helmet = require('helmet')
+const corsOptions = require('./config/development.cjs')
+const readFile = util.promisify(fs.readFile);
 
-const rest = require('./routes/api/rest.js');
-const soap = require('./routes/api/soap.js');
+//endpoints
+// const rest = require('./routes/api/rest.js');
+// const soap = require('./routes/api/soap.js');
 
 
 app.use(helmet());
 app.use(bodyParser.json());
-
-
+app.use(cors(corsOptions || {}))
+app.disable("x-powered-by");
 //Adding endpoints
-app.use('/rest', rest);
-app.use('/soap', soap);
+// app.use('/rest', rest);
+// app.use('/soap', soap);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
-const server = require('http').createServer(app);
+// const server = require('http').createServer(app);
 
 const port = process.env.PORT || 3001
-console.log('Server is working on port ' + port);
-server.listen(port)
+const  start = async () => {
+	const [key, cert] = await Promise.all([
+		readFile('_security/key.pem','ascii'),
+		readFile('_security/cert.pem','ascii'),
+	]);
+	console.log(key);
+	if (key && cert) {
+		https
+			.createServer({ key, cert }, app)
+			.listen(port, () => console.log("HTTPS server UP - https://localhost:" + port));
+	} else {
+		console.error("Cannot start server, key or cert not found.");
+	}
+}
+
+
+start()
