720a0dce8eaeb2549fa04423a076635bc73539ee
Patryk <ngldiztadhdbtndapf@kiabws.online>
2021-06-13 17:45:27 +0200

    Small refactor in order.js

--- routes/api/order.js
+++ routes/api/order.js
@@ -1,32 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const db = require("../../models/index");
-const Order = db.order
+const Order = db.order;
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     Order.findAll()
-    .then(data => res.send(data))
+        .then(data => res.send(data))
 
-})
+});
 
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
     const firstOrder = {
         orderDate: new Date(),
         payDate: new Date(),
         status: 'Do zapłacenia',
         table: '4',
         currency: 'test',
-    }
+    };
     Order.create(firstOrder)
-    .then(data => res.send(data))
-})
+        .then(data => res.send(data))
+});
 
-router.put('/', (req,res) => {
+router.put('/', (req, res) => {
 
-})
+});
 
-router.delete('/', (req,res) => {
+router.delete('/', (req, res) => {
 
-})
+});
 
-module.exports = router
+module.exports = router;
