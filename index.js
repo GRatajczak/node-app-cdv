0ecbb9ca869afe6238c13d21c3214eb8c2c18f03
Grzegorz Ratajczak <grzegorz@codium.studio>
2021-06-16 13:55:27 +0200

    added set order enpoint

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ require('dotenv').config();
 // const menu = require('./routes/api/menu.js');
 const order = require('./routes/api/order.js');
 const orderedDish = require('./routes/api/ordered-dish.js');
+const setOrder = require('./routes/api/set-order.js');
 
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions || {}));
 app.disable("x-powered-by");
 
@@ -29,11 +31,12 @@ app.disable("x-powered-by");
 // app.use('/menu', menu);
 app.use('/order', order);
 app.use('/orderedDish', orderedDish);
+app.use('/set-order', setOrder);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 const  start = async () => {
 	const [key, cert] = await Promise.all([
