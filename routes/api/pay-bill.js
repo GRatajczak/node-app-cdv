cf1b1fe93b3ce8b55949b4ae5b97238bc70e76b7
Grzegorz Ratajczak <grzegorz@codium.studio>
2021-06-17 23:00:20 +0200

    added pay bill endpoint

--- routes/api/pay-bill.js
+++ routes/api/pay-bill.js
@@ -2,17 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require("../../models/index");
 const Order = db.order;
+const OrderedDish = db.orderedDish;
 const Bill = db.bill;
 const axios = require('axios');
 let  menuIsCompleted = require('../../config/checkMenuStatus');
-const { toLower } = require('lodash');
 
 
 
 router.put('/', async (req, res) => {
     const transaction = await db.sequelize.transaction();
     let { orderId } = req.body;
-
   
     let menuStatus = await menuIsCompleted();
     if(menuStatus){
@@ -20,24 +19,28 @@ router.put('/', async (req, res) => {
             {where:{id: orderId}},
             {transaction}
         )
-
         let currencyToPay = currentBill.dataValues.currency
-        console.log(`http://api.nbp.pl/api/exchangerates/rates/a/${currentBill.dataValues.currency}/today`);
+        let currencyBid = 1;
         if(currencyToPay !== 'PLN'){
-            // const currency = await axios.get(`http://api.nbp.pl/api/exchangerates/rates/c/${'usd'}/today`)
-            // .then(el => console.log(el.data.rates))
-            // console.log(currency.data.rates);
+            const currency = await axios.get(`http://api.nbp.pl/api/exchangerates/rates/c/${currencyToPay}/today`)
+            currencyBid = currency.data.rates[0].bid;
         }
-        // await Order.update(
-        //     {status: "Zapłacone", payDate: new Date},
-        //     {returning: true, where: {id: orderId},},
-        //     {transaction}
-        // )
+        let toPay = 0;
+        const allDishes = await OrderedDish.findAll({where: {orderId: orderId}}, {transaction})
+        allDishes.forEach(el => {
+            toPay += el.dataValues.price
+            console.log(el.dataValues.price);
+            console.log(currencyBid);
+        })
+        const billId = allDishes[0].dataValues.billId;
 
+        await Bill.update(
+            {paidUp: true},
+            {returning: true, where: {id: billId},},
+            {transaction}
+        )
         await transaction.commit();
-        res.status(200).send('Zamówienie zostało opłacone!')
-
-      
+        res.status(200).send({msg: `do zapłaty ${(toPay / currencyBid).toFixed(2)} ${currencyToPay}`})
     }else {
         res.status(200).send('Za mało pozycji w menu!')
     }
