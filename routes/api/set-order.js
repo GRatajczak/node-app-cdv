0d8d526584000ec3623a5c005287e97de613546f
Grzegorz Ratajczak <grzegorz@codium.studio>
2021-06-18 18:07:05 +0200

    finished set order endpoint

--- routes/api/set-order.js
+++ routes/api/set-order.js
@@ -10,8 +10,8 @@ let  menuIsCompleted = require('../../config/checkMenuStatus')
 
 router.post('/', async (req, res) => {
     const transaction = await db.sequelize.transaction();
-
-    let { status, table, currency, dishName, price, category, description, dishes } = req.body;
+    const arrayOfDishes = [];
+    let { table, currency, dishes } = req.body;
     
     if(currency !== 'EUR' && currency !== 'USD'){
         currency = 'PLN'
@@ -21,36 +21,42 @@ router.post('/', async (req, res) => {
         let menuStatus = await menuIsCompleted();
         if(menuStatus){
             
-
             const newBill  = await Bill.create({paidUp: false, currency},{transaction})
-
             const newOrder = await Order.create(
             {
                 orderDate: new Date,
                 payDate: null,
-                status,
+                status: 'Złożone',
                 table
             }, 
             {transaction}
             )
-            const test = await  Promise.each([1,2,3,4], async (item) => {
-            const transaction2 = await db.sequelize.transaction();
-                await OrderedDish.create({
-                    orderId: newBill.id,
-                    billId: newOrder.id,
-                    dishName,
-                    price,
-                    category,
-                    description,
-                }, {transaction2})
-            await transaction2.commit();
 
-
+            await  Promise.each(JSON.parse(dishes), async (item) => {
+                const transaction2 = await db.sequelize.transaction();
+                const dishFromMenu = await Menu.findOne({where: {id: item.id}},{transaction2})
+                if(!dishFromMenu){
+                    arrayOfDishes.push({id: item.id})
+                }
+                if(dishFromMenu){
+                    await OrderedDish.create({
+                        orderId: newBill.id,
+                        billId: newOrder.id,
+                        dishName: dishFromMenu.dataValues.dishName,
+                        price:  dishFromMenu.dataValues.price_PLN,
+                        category: dishFromMenu.dataValues.category,
+                        description: dishFromMenu.dataValues.description,
+                    }, {transaction2})
+                    await transaction2.commit();
+                }
             })
 
             await transaction.commit();
 
-            res.status(201).send({msg: "Zamówienie zostało złożone", menuElements: []})
+            res.status(201).send({msg: "Zamówienie zostało złożone", notInMenu: {
+                msg: 'W menu nie ma takich potraw',
+                dishes: arrayOfDishes
+            }})
         }else {
             res.status(200).send('Za mało pozycji w menu!')
         }
