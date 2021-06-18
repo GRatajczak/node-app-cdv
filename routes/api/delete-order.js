e4c3eaeac54236e3e4cfb2ff3f561bb22693e1e0
Patryk <ngldiztadhdbtndapf@kiabws.online>
2021-06-18 21:32:08 +0200

    Fix delete-order.js

--- routes/api/delete-order.js
+++ routes/api/delete-order.js
@@ -4,19 +4,19 @@ const db = require("../../models/index");
 const OrderedDish = db.orderedDish;
 const Bill = db.bill;
 
-router.delete('/:id', async (req, res) => {
+router.delete('/', async (req, res) => {
     const transaction = await db.sequelize.transaction();
     try {
-        await Bill.delete({id: req.query.id}, {transaction});
-        await OrderedDish.delete({billId: req.query.id});
+        await Bill.destroy({where: {id: req.query.id}, transaction: transaction});
+        await OrderedDish.destroy({where: {billId: req.query.id}, transaction: transaction});
         await transaction.commit();
 
-        res.status(201).send({msg: "Zamówienie zostało anulowane"})
+        res.status(204).send({msg: "Zamówienie zostało anulowane"})
     } catch (error) {
+        console.log(error);
         await transaction.rollback();
         res.status(500).send(error);
     }
 });
-
 
 module.exports = router;
