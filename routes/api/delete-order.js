0e9d2017f6328f75283100e8a18178d528577364
Patryk <ngldiztadhdbtndapf@kiabws.online>
2021-06-19 12:12:54 +0200

    Fix delete-order.js

--- routes/api/delete-order.js
+++ routes/api/delete-order.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../../models/index");
+const Order = db.order;
 const OrderedDish = db.orderedDish;
 const Bill = db.bill;
 
 router.delete('/', async (req, res) => {
     const transaction = await db.sequelize.transaction();
     try {
-        await Bill.destroy({where: {id: req.query.id}, transaction: transaction});
-        await OrderedDish.destroy({where: {billId: req.query.id}, transaction: transaction});
-        await transaction.commit();
+        const orderId = req.query.id;
+        await Order.destroy({where: {id: orderId}, transaction: transaction});
+        const orderedDishes = await OrderedDish.find({where: {orderId: orderId}}, {transaction});
+        const involvedBillsIds = new Set(orderedDishes.map(order => order.billId));
+        await OrderedDish.destroy({where: {orderId: orderId}, transaction: transaction});
+        involvedBillsIds.forEach(await async function (billId) {
+            const isBillInUsage = await OrderedDish.findOne({where: {billId: billId}}, transaction) != null;
+            if (isBillInUsage) {
+                Bill.destroy({where: {id: billId}, transaction: transaction})
+            }
+        });
 
+        await transaction.commit();
         res.status(204).send({msg: "Zamówienie zostało anulowane"})
     } catch (error) {
         console.log(error);
