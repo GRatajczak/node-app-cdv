f552ac20e688157bab0b1b54130f766d4200c978
Patryk <ngldiztadhdbtndapf@kiabws.online>
2021-06-19 12:42:08 +0200

    Fix delete-order.js

--- routes/api/delete-order.js
+++ routes/api/delete-order.js
@@ -6,25 +6,29 @@ const OrderedDish = db.orderedDish;
 const Bill = db.bill;
 
 router.delete('/', async (req, res) => {
-    const transaction = await db.sequelize.transaction();
+    const transactionOne = await db.sequelize.transaction();
+    const transactionTwo = await db.sequelize.transaction();
     try {
         const orderId = req.query.id;
-        await Order.destroy({where: {id: orderId}, transaction: transaction});
-        const orderedDishes = await OrderedDish.find({where: {orderId: orderId}}, {transaction});
+        await Order.destroy({where: {id: orderId}, transaction: transactionOne});
+        const orderedDishes = await OrderedDish.findAll({where: {orderId: orderId}, transaction : transactionOne});
         const involvedBillsIds = new Set(orderedDishes.map(order => order.billId));
-        await OrderedDish.destroy({where: {orderId: orderId}, transaction: transaction});
+        await OrderedDish.destroy({where: {orderId: orderId}, transaction: transactionOne});
+
         involvedBillsIds.forEach(await async function (billId) {
-            const isBillInUsage = await OrderedDish.findOne({where: {billId: billId}}, transaction) != null;
-            if (isBillInUsage) {
-                Bill.destroy({where: {id: billId}, transaction: transaction})
+            const isBillInUsage = await OrderedDish.findOne({where: {billId: billId}}, transactionTwo) != null;
+            if (!isBillInUsage) {
+                Bill.destroy({where: {id: billId}, transaction: transactionTwo})
             }
         });
 
-        await transaction.commit();
+        await transactionTwo.commit();
+        await transactionOne.commit();
         res.status(204).send({msg: "Zamówienie zostało anulowane"})
     } catch (error) {
         console.log(error);
-        await transaction.rollback();
+        await transactionTwo.rollback();
+        await transactionOne.rollback();
         res.status(500).send(error);
     }
 });
